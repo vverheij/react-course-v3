dd40289112a82c78d4b2a3192ea3521d5948c1d7
Victor Verheij <v.verheij@gmail.com>
2025-03-18 23:21:49 +0100

    finished html setup in app.js

--- 02-backroads-project/backroads-app/src/main.jsx
+++ 02-backroads-project/backroads-app/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+// in create react app this file is index.js
+// in create react app this file is also importing index.css
+// strict mode only runs on dev and renders everything twice;
+// console.log() statemenst in components appear twice because of that
