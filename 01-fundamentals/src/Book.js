a5544a2111778fd08e509c6e135a0c889e6ba8e8
Victor Verheij <v.verheij@gmail.com>
2025-03-18 08:08:20 +0100

    vite setup of backroads app finished

--- 01-fundamentals/src/Book.js
+++ 01-fundamentals/src/Book.js
@@ -1,14 +1,14 @@
-const Book = (props) => {
-  const { img, title, author } = props;
-
+const Book = ({ img, title, author, id, getBook }) => {
+  const getSingleBook = () => {
+    getBook(id);
+  };
   return (
-    <article className='book'>
+    <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
-
       <h4>{author} </h4>
+      <button onClick={getSingleBook}>Click Me</button>
     </article>
   );
 };
-
 export default Book;
--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -7,18 +7,46 @@ import { books } from './books';
 import Book from './Book';
 
 function BookList() {
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
+  };
   return (
-    <>
-      <h1>amazon best sellers</h1>
-      <section className="booklist">
-        {books.map((book) => {
-          return <Book {...book} key={book.id} />;
-        })}
-      </section>
-    </>
+    <section className="booklist">
+      <EventExamples />
+      {books.map((book) => {
+        return <Book {...book} getBook={getBook} key={book.id} />;
+      })}
+    </section>
   );
 }
-
+function EventExamples() {
+  const handelFormInput = (e) => {
+    console.log('e:', e.target);
+    console.log('e.target.name:', e.target.name);
+    console.log('e.target.value:', e.target.value);
+    // console.log('form input handled');
+  };
+  const handleButtonClick = () => {
+    alert('button click handled');
+  };
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    console.log('form submitted');
+  };
+  return (
+    <section onSubmit={handleFormSubmission}>
+      <form>
+        <h2>Typical form</h2>
+        <input type="text" name="example" onChange={handelFormInput} style={{ margin: '1rem 0' }} />
+        <button type="submit">Submit</button>
+      </form>
+      <button type="button" onClick={handleButtonClick}>
+        Click Me
+      </button>
+    </section>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<BookList />);
