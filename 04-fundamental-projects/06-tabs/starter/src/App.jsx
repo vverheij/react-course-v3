d72b6949ce63edbd06b7037ec2f2924b50263522
Victor Verheij <v.verheij@gmail.com>
2025-03-26 07:53:26 +0100

    beginning slider

--- 04-fundamental-projects/06-tabs/starter/src/App.jsx
+++ 04-fundamental-projects/06-tabs/starter/src/App.jsx
@@ -1,6 +1,56 @@
+import { useEffect, useState } from 'react';
+import Title from './Title';
+import JobInfo from './JobInfo';
+import BtnContainer from './BtnContainer';
 const url = 'https://www.course-api.com/react-tabs-project';
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+  const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentItem, setCurrentItem] = useState(1);
+
+  // const setCurrentItemHandler = (id) => {
+  //   console.log(id);
+  //   setCurrentItem(id);
+  // };
+
+  const fetchJobs = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        console.log(response);
+        return;
+      }
+      const result = await response.json();
+      setJobs(result);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    fetchJobs();
+    return () => {};
+  }, []);
+
+  {
+    /* <Title text="Full Stack Web Developer" /> */
+  }
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="jobs-center">
+      {/* button container */}
+      <BtnContainer jobs={jobs} currentItem={currentItem} setCurrentItem={setCurrentItem} />
+      <JobInfo jobs={jobs} currentItem={currentItem} />
+    </section>
+  );
 };
 export default App;
---
+++ 04-fundamental-projects/06-tabs/starter/src/JobInfo.jsx
@@ -0,0 +1,13 @@
+import Duties from './Duties';
+const JobInfo = ({ jobs, currentItem }) => {
+  const { id, title, company, dates, duties } = jobs[currentItem];
+  return (
+    <article className="job-info">
+      <h3>{title}</h3>
+      <span className="job-company">{company}</span>
+      <p className="job-date">{dates}</p>
+      <Duties duties={duties} />
+    </article>
+  );
+};
+export default JobInfo;
