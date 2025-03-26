53064f8f43b726c4e5f22ecb46bbfef47b06856e
Victor Verheij <v.verheij@gmail.com>
2025-03-26 08:05:36 +0100

    wip

--- 04-fundamental-projects/07-slider/starter/src/App.jsx
+++ 04-fundamental-projects/07-slider/starter/src/App.jsx
@@ -1,4 +1,11 @@
+import { useState } from 'react';
+import { shortList, list, longList } from './data';
+
 const App = () => {
+  const [carousel, setCarousel] = useState(shortList);
+  console.log(shortList);
+  console.log(carousel);
+
   return (
     <main>
       <h2>Slider Starter</h2>
